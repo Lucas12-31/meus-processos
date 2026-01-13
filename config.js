cbf267753e1e607d1d81e5722d7ef4c5c12bd024
Lucas12-31 <lmlimoeiro@gmail.com>
2026-01-12 21:48:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -14,4 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app); // NOVO: Exportamos a autenticação
+export const auth = getAuth(app); 
